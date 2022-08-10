40776dea277957cf5d150716eedbf366d9796681
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-09 21:45:26 -0600

    editing some more on code and began README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Coding-Quiz
+
+
+## The Quiz
+
+As the user, you should be able to Open the game and take a timed quiz that will store your scores
+
+## The Journey 
+
+    * Begin by pressing the start button. At this point in time you should see the time start counting down.
+    * You will then be presented with a question that has only 1 correct answer.
+    * However you will be given 4 similar sounding answers.
+    * The game will end when the timer is done or you have answered the questions correctly.
+
+## ScreenShots
+
+``coming soon``
+
+## Installation 
+
+This should be a pretty straightforward installation.
+
--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const startButton = document.getElementById('start-btn')
-const questionConatinerElement = document.getElementById('question-container')
+var startButton = document.getElementById('start-btn')
+var questionConatinerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question');
+var answerButtonsElement = document.getElementById('answer-buttons')
 
+var shuffledQustions = "";
+var currentQuestionIndex = "";
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
+
     startButton.classList.add('hide')
-    questionConatinerElement.classList.remove('hide')
+    //This is what shuffles the questions
+    shuffledQustions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionConatinerElement.classList.remove('hide');
     setNextQuestion()
 
+    
+    
+    
+    function setNextQuestion() {
+        //I want to show the questions that are shuffled through the currentQuestionIndex
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
+    }
+    
+    function showQuestion(question) {
+        questionElement.innerText = question.question
+        
+    }
+    
+    
+    function selectAnswer() {
+        
+    }
+    
+    var questions = [
+        {
+            question: "What is the first letter of the alphabet",
+            answers: [
+                {text: "A", correct: true},
+                {text: '22', correct: false}
+            ]
+        }
+    ]
 }
-
-
-
-function setNextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
-
