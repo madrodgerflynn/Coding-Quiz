fa6f9ff780b56a585801edfb016fee47245225b7
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-11 16:33:59 -0600

    working JS

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
-var startButton = document.getElementById("start-btn");
+var beginButton = document.getElementById("begin-quiz");
 var questionConatinerElement = document.getElementById("question-container");
 var questionElement = document.getElementById("question");
 var answerButtonsElement = document.getElementById("answer-buttons");
+var runningQuestionIndex = 0;
 
 var questions = [
-    "What is the 13th letter of the alphabet",
-    "Who was the 3rd President of the USA",
-    "Who was the first woman in space?",
+  {
+    question: "What is the 13th letter of the alphabet",
+    choiceA: "F",
+    choiceB: "A",
+    choiceC: "Z",
+    correct: "M",
+  },
+  {
+    question: "Who was the 3rd President of the USA",
+    choiceA: "John Adams",
+    choiceB: "James Madison",
+    choiceC: "John Quincy Adams",
+    correct: "Thomas Jefferson",
+  },
+  {
+    question: "Who was the first woman in space?",
+    choiceA: "Svetlana Yevgenyevna Savitskaya",
+    choiceB: "Serena M. Auñón-Chancello",
+    choiceC: "Samantha Cristoforetti",
+    correct: "Valentina Tereshkova",
+  },
 ];
 
-var correctAnswers = [
-    "M", "Thomas Jefferson", "Valentina Tereshkova"
-];
+beginButton.addEventListener("click", beginQuiz);
 
-var incorrectAnswers = [
-    "Z",
-    "A",
-    "F",
-    "John Adams",
-    "James Madison",
-    "John Quincy Adams",
-    "Svetlana Yevgenyevna Savitskaya",
-    "Serena M. Auñón-Chancello",
-    "Samantha Cristoforetti",
-
-];
-
-
-function StartBtn() {
-    generateBtn.addEventListener('click', beginQuiz);
-
- function beginQuiz() {
-    var  questionConatinerElement = questions[0];
-       
-        console.log(questionConatinerElement);
-    // var answerButtonsElement = correctAnswers
-    
-
-    
-    
-
-
+function beginQuiz() {
+  console.log(answerButtonsElement);
+  //want to populate the question container
+  nextQuestion();
 }
+function nextQuestion() {
+  if (questions[runningQuestionIndex]) {
+    questionElement.innerHTML =
+      "<p>" + questions[runningQuestionIndex].question + "</p>";
+    answerButtonsElement.children[0].innerHTML =
+      questions[runningQuestionIndex].choiceA;
+    answerButtonsElement.children[1].innerHTML =
+      questions[runningQuestionIndex].choiceB;
+    answerButtonsElement.children[2].innerHTML =
+      questions[runningQuestionIndex].choiceC;
+    answerButtonsElement.children[3].innerHTML =
+      questions[runningQuestionIndex].correct;
+
+    runningQuestionIndex++;
+  }
+  else {
+
+    console.log("endOfQuiz")
+  }
 }
+function endOfQuiz () {
+
+    //presents score
+    //stop timer
+    //type initials
+}
+
+
+$(".answer").on("click", nextQuestion);
