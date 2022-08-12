2c495429a366d4cbbf9a0db15928fd49528f8392
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-11 20:25:02 -0600

    JS change but still working

--- script.js
+++ script.js
@@ -3,64 +3,128 @@ var questionConatinerElement = document.getElementById("question-container");
 var questionElement = document.getElementById("question");
 var answerButtonsElement = document.getElementById("answer-buttons");
 var runningQuestionIndex = 0;
-
+var score = 0;
+var globalTime = 60;
 var questions = [
   {
     question: "What is the 13th letter of the alphabet",
     choiceA: "F",
+    correct: "M",
     choiceB: "A",
     choiceC: "Z",
-    correct: "M",
   },
   {
     question: "Who was the 3rd President of the USA",
+    correct: "Thomas Jefferson",
     choiceA: "John Adams",
     choiceB: "James Madison",
     choiceC: "John Quincy Adams",
-    correct: "Thomas Jefferson",
   },
   {
     question: "Who was the first woman in space?",
     choiceA: "Svetlana Yevgenyevna Savitskaya",
     choiceB: "Serena M. Auñón-Chancello",
-    choiceC: "Samantha Cristoforetti",
     correct: "Valentina Tereshkova",
+    choiceC: "Samantha Cristoforetti",
   },
 ];
 
-beginButton.addEventListener("click", beginQuiz);
+
 
 function beginQuiz() {
   console.log(answerButtonsElement);
+  
   //want to populate the question container
   nextQuestion();
+  startTimer();
 }
-function nextQuestion() {
+
+beginButton.addEventListener("click", beginQuiz);
+
+function nextQuestion(e) {
+
   if (questions[runningQuestionIndex]) {
-    questionElement.innerHTML =
-      "<p>" + questions[runningQuestionIndex].question + "</p>";
-    answerButtonsElement.children[0].innerHTML =
-      questions[runningQuestionIndex].choiceA;
-    answerButtonsElement.children[1].innerHTML =
-      questions[runningQuestionIndex].choiceB;
-    answerButtonsElement.children[2].innerHTML =
-      questions[runningQuestionIndex].choiceC;
-    answerButtonsElement.children[3].innerHTML =
-      questions[runningQuestionIndex].correct;
+    // questionElement.innerHTML =
+    //   "<p>" + questions[runningQuestionIndex].question + "</p>";
+    // answerButtonsElement.children[0].innerHTML =
+    //   questions[runningQuestionIndex][Object.keys(questions[runningQuestionIndex])[1]];
+    // answerButtonsElement.children[1].innerHTML =
+    //   questions[runningQuestionIndex][Object.keys(questions[runningQuestionIndex])[2]];
+    // answerButtonsElement.children[2].innerHTML =
+    //   questions[runningQuestionIndex][Object.keys(questions[runningQuestionIndex])[3]];
+    // answerButtonsElement.children[3].innerHTML =
+    //   questions[runningQuestionIndex][Object.keys(questions[runningQuestionIndex])[4]];
+    //  var currentObject = (Object.entries(questions[runningQuestionIndex]));
+    //  console.log(currentObject);
+     
+    //   for (var i = 0; i < 5; i++) {
+    //  if (currentObject[i][1] == answerButtonsElement.children[0].innerHTML) {
+    //     console.log(currentObject[i]);
+    //      console.log(true);
+    //     }
+    //  }
+
+    const currentObject = Object.entries(questions[runningQuestionIndex]);
+
+    questionElement.textContent = currentObject[0][1];
+
+    for (let i = 1; i < currentObject.length; i++) {
+        const button = document.createElement('button');
+        // console.log(currentObject[0][1]);
+        button.textContent = currentObject[i][1];
+        answerButtonsElement.appendChild(button);
+        button.addEventListener('click', (e) => {
+            console.log(questions[runningQuestionIndex -1].correct);
+            var response =  e.target.textContent
+            if (questions[runningQuestionIndex -1].correct == response){
+                score ++; 
+            }  
+            else
+            globalTime -=10;
+                
+            
+            
+            $("#answer-buttons").empty();
+            nextQuestion();
+        });
+        
+    }
+
 
     runningQuestionIndex++;
-  }
-  else {
-
-    console.log("endOfQuiz")
+  } else {
+    console.log();
+    endOfQuiz();
   }
 }
-function endOfQuiz () {
-
-    //presents score
-    //stop timer
-    //type initials
+function endOfQuiz() {
+  alert("The Quiz is over!");
 }
 
 
-$(".answer").on("click", nextQuestion);
+function startTimer() {
+ 
+    var display = document.getElementById("time");
+  var timerElement = setInterval(function () {
+    globalTime--;
+
+    display.textContent = globalTime;
+
+    if (globalTime <= 0) {
+      clearInterval(timerElement);
+    }
+    
+  }, 1000);
+}
+
+// $(".answer").on("click", nextQuestion);
+
+const test = document.querySelectorAll(".answer");
+// console.log(buttonTest);
+
+// const test = $(".answer");
+// for (let i = 0; i <test.length; i++) {
+//     const currentButton = test[i];
+//     console.log(currentButton);
+//     currentButton.addEventListener('click', nextQuestion);
+// }
